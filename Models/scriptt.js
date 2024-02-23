77958ec9228715be37fa81705099ed23d1648e8e
Juan Diaz <juanjose041223@gmail.com>
2024-02-23 12:57:02 -0600

    Cambios Index
    Agregue cuenta, Modifique el script, estilo y el index

--- Models/scriptt.js
+++ Models/scriptt.js
@@ -28,3 +28,70 @@ document.addEventListener('DOMContentLoaded', function() {
     filterButtonInsideSidebar.addEventListener('click', toggleSidebar);
   }
 });
+
+
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
+}
+
+
+
+
+
+window.onload = function() {
+  if (localStorage.getItem('profileImage')) {
+      document.getElementById('profileImage').src = localStorage.getItem('profileImage');
+  }
+  
+  if (localStorage.getItem('userDescription')) {
+      document.getElementById('userDescription').textContent = localStorage.getItem('userDescription');
+  }
+};
+
+document.getElementById('profileImage').addEventListener('click', function() {
+  document.getElementById('imageInput').click();
+});
+
+document.getElementById('imageInput').addEventListener('change', updateProfileImage);
+
+function updateProfileImage() {
+  const input = document.getElementById('imageInput');
+  const profileImage = document.getElementById('profileImage');
+  if (input.files && input.files[0]) {
+      let reader = new FileReader();
+      reader.onload = function(e) {
+          profileImage.src = e.target.result;
+          localStorage.setItem('profileImage', e.target.result);
+      };
+      reader.readAsDataURL(input.files[0]);
+  }
+}
+
+document.getElementById('editDescriptionBtn').addEventListener('click', showDescriptionInput);
+
+function showDescriptionInput() {
+  document.getElementById('descriptionInputContainer').classList.remove('hidden');
+  document.getElementById('userDescription').classList.add('hidden');
+  document.getElementById('editDescriptionBtn').classList.add('hidden');
+  document.getElementById('descriptionInput').value = document.getElementById('userDescription').textContent;
+}
+
+function updateDescription() {
+  const descriptionInput = document.getElementById('descriptionInput');
+  const userDescription = document.getElementById('userDescription');
+  userDescription.textContent = descriptionInput.value;
+  localStorage.setItem('userDescription', descriptionInput.value);
+  document.getElementById('descriptionInputContainer').classList.add('hidden');
+  userDescription.classList.remove('hidden');
+  document.getElementById('editDescriptionBtn').classList.remove('hidden');
+}
